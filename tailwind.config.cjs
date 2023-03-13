4dcbaea541f255fb62dd522f80340af1ef36f030
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-13 22:47:12 +0530

    Add primary color to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f0faff",
+          100: "#e1f4fd",
+          200: "#bce9fb",
+          300: "#81daf8",
+          400: "#3ec7f2",
+          500: "#15b4e7",
+          600: "#088fc1",
+          700: "#08729c",
+          800: "#0b6081",
+          900: "#0f4f6b",
+        },
+      },
+    },
   },
   plugins: [],
 };
