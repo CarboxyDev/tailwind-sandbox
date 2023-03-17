16a6eb62d63c409d499c1ed1cfe432e4fb973bec
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-18 00:13:10 +0530

    Add minor changes to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,20 +12,22 @@ const Navbar = () => {
     <>
       <div className="flex h-24 flex-row items-center px-4 sm:h-32 sm:px-8 lg:px-28">
         <div className="flex flex-row items-center gap-x-3">
-          <img src="/generic-logo-1.png" className="h-12 w-12" alt="logo" />
-          <h2 className="text-lg font-semibold text-primary">charitylake</h2>
+          <Link href="/">
+            <img src="/generic-logo-1.png" className="h-12 w-12" alt="logo" />
+          </Link>
+          <h2 className="text-lg font-semibold text-slate-700">charitylake</h2>
         </div>
         <div className="ml-16 hidden list-none flex-row items-center gap-x-9 lg:flex">
           {navItems.map((item) => {
             return (
-              <li className="rounded-md px-2 py-1 text-sm text-slate-500 hover:bg-slate-50 hover:text-slate-700">
+              <li className="rounded-md px-2 py-1 text-sm text-slate-600 hover:bg-slate-50 hover:text-slate-700">
                 <Link href={item.link}>{item.name}</Link>
               </li>
             );
           })}
         </div>
         <div className="ml-auto flex flex-row gap-x-8">
-          <button className="hidden h-12 w-32 items-center justify-center rounded-lg bg-primary text-sm font-semibold text-white sm:flex">
+          <button className="hidden h-12 w-32 items-center justify-center rounded-lg bg-primary text-sm font-semibold text-white transition-colors delay-100 ease-in hover:bg-primary-400 sm:flex">
             Sign up
           </button>
           <Bars3Icon className="h-12 w-12 text-[#545454] lg:hidden" />
