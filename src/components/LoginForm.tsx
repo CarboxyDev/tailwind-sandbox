1ae4252eb1bac04d83422e8a0216586111843cf0
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-18 00:05:59 +0530

    Add minor styling changes to LoginForm

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { GithubIcon, GoogleIcon, TwitterIcon } from "../utils/icons";
 
 const LoginForm = () => {
@@ -8,18 +9,20 @@ const LoginForm = () => {
           <p className="font-medium text-gray-600">Email address</p>
           <input
             type="email"
-            className="mt-3 h-10 rounded-lg border border-gray-300 px-2 focus:outline-primary"
+            className="mt-3 h-10 rounded-lg border border-gray-300 px-2 text-gray-700 focus:outline-primary"
           />
         </div>
         <div className="mt-8 flex flex-col">
           <p className="font-medium text-gray-600">Password</p>
           <input
             type="password"
-            className="mt-3 h-10 rounded-lg border border-gray-300 px-2 focus:outline-primary"
+            className="mt-3 h-10 rounded-lg border border-gray-300 px-2 text-gray-700 focus:outline-primary"
           />
         </div>
         <div className="mt-6 flex justify-end">
-          <span className="text-gray-500">Forgot your password?</span>
+          <Link href="/" className="text-sm text-gray-500 hover:underline">
+            Forgot your password?
+          </Link>
         </div>
         <div className="mt-8 h-11 w-full">
           <button className="flex h-full w-full items-center justify-center rounded-lg bg-primary text-white transition-colors delay-100 ease-in hover:bg-primary-600">
