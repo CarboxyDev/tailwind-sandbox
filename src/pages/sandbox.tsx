09af846e59a9228d2a10e2281b29f1b943bd6895
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-18 06:59:06 +0530

    Add CardWithImageAndButton component

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -13,6 +13,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import LoginForm from "../components/LoginForm";
 import CardWithImage from "../components/CardWithImage";
+import CardWithImageAndButton from "../components/CardWithImageAndButton";
 
 const Sandbox: NextPage = () => {
   return (
@@ -21,6 +22,15 @@ const Sandbox: NextPage = () => {
       <div className="px-12 pt-12">
         <div className="pt-24"></div>
         <div></div>
+        <div className="pt-80 pb-80"></div>
+        <div>
+          <CardWithImageAndButton
+            image="/generic-image-1.png"
+            heading="Some Heading"
+            content="This is some example text for this card design. Let's fit some more content in here so that it feels not-so-empty. Alright, that's enough for now."
+            buttonText="Learn more"
+          />
+        </div>
         <div className="pt-80 pb-80"></div>
         <div className="2xl:grids-cols-4 grid grid-cols-1 gap-x-4 gap-y-4 md:grid-cols-2 xl:grid-cols-3">
           <CardWithImage
