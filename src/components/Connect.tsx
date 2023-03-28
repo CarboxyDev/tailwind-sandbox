ac17e3a0186d9bed2efb20eb324ddf8d3fe4f8e3
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-28 21:50:34 +0530

    Add changes to Connect.tsx + Add misc/randint method

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,15 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { randint } from "../utils/misc";
+
+type connectType = "connecting" | "success" | "failure";
 
 const Connect = () => {
+  const [connected, setConnected] = useState<connectType>("connecting");
+
   useEffect(() => {
-    //
-  }, []);
+    setTimeout(() => {
+      const r = randint(1, 2);
+      if (r == 1) {
+        setConnected("failure");
+      } else {
+        setConnected("success");
+      }
+    }, 4000);
+  }, [connected]);
 
   return (
     <>
       <div className="inline-flex min-w-[400px] items-center rounded-2xl border border-gray-200 bg-slate-50 px-6 py-8">
-        <div className="">
-          <span className="font-semibold text-slate-500">Connecting...</span>
+        <div>
+          {connected == "connecting" && (
+            <span className="font-semibold text-slate-600">Connecting...</span>
+          )}
+          {connected == "success" && (
+            <span className="font-semibold text-green-500">Connected</span>
+          )}
+          {connected == "failure" && (
+            <span className="font-semibold text-red-500">
+              Unable to connect
+            </span>
+          )}
         </div>
       </div>
     </>
--- src/utils/misc.tsx
+++ src/utils/misc.tsx
@@ -3,3 +3,7 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...classes: ClassValue[]) => twMerge(clsx(...classes));
+
+export const randint = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
