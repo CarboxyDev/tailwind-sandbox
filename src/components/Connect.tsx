fc47f4c854fa22dc33bc21200167e35c1f4336c6
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-03-28 22:17:49 +0530

    Complete Connect component

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { randint } from "../utils/misc";
+import { Rings } from "svg-loaders-react";
+import clsx from "clsx";
+import { CheckIcon } from "../utils/icons";
 
 type connectType = "connecting" | "success" | "failure";
 
@@ -7,6 +10,7 @@ const Connect = () => {
   const [connected, setConnected] = useState<connectType>("connecting");
 
   useEffect(() => {
+    // simulate an api call for some hypothetical connection
     setTimeout(() => {
       const r = randint(1, 2);
       if (r == 1) {
@@ -14,24 +18,42 @@ const Connect = () => {
       } else {
         setConnected("success");
       }
-    }, 4000);
+    }, 2000);
   }, [connected]);
 
   return (
     <>
-      <div className="inline-flex min-w-[400px] items-center rounded-2xl border border-gray-200 bg-slate-50 px-6 py-8">
-        <div>
-          {connected == "connecting" && (
-            <span className="font-semibold text-slate-600">Connecting...</span>
-          )}
-          {connected == "success" && (
-            <span className="font-semibold text-green-500">Connected</span>
-          )}
-          {connected == "failure" && (
-            <span className="font-semibold text-red-500">
-              Unable to connect
-            </span>
-          )}
+      <div className="h-18 w-80 sm:w-100">
+        <div className="flex h-full w-full flex-row items-center rounded-2xl border border-gray-200 bg-slate-50 px-6 py-6">
+          <div>
+            {connected == "connecting" && (
+              <span className="font-semibold text-slate-600">
+                Connecting...
+              </span>
+            )}
+            {connected == "success" && (
+              <span className="font-semibold text-green-500">Connected</span>
+            )}
+            {connected == "failure" && (
+              <span className="font-semibold text-red-500">
+                Unable to connect
+              </span>
+            )}
+          </div>
+          <div className="ml-auto">
+            {connected != "success" && (
+              <Rings
+                className={clsx(
+                  "stroke-slate-500",
+                  connected == "connecting" && "visible",
+                  connected == "failure" && "invisible"
+                )}
+              />
+            )}
+            {connected == "success" && (
+              <CheckIcon className="h-6 w-6 text-green-500"></CheckIcon>
+            )}
+          </div>
         </div>
       </div>
     </>
