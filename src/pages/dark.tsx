08697ba7dfe55f08c5680d715cc609a6e4b2a225
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-04-08 23:56:41 +0530

    Add minor changes to dark.tsx

--- src/pages/dark.tsx
+++ src/pages/dark.tsx
@@ -7,7 +7,12 @@ const Dark: NextPage = () => {
       <Head>
         <title>Tailwind Dark Sandbox</title>
       </Head>
-      <main className=""></main>
+      <main className="min-h-screen bg-gray-900">
+        <div className="flex flex-col pl-20 pt-20">
+          <div className="h-100 w-100 rounded-2xl bg-gray-800 shadow-md"></div>
+          <div className="mt-10 h-100 w-100 rounded-2xl bg-slate-800 shadow-md"></div>
+        </div>
+      </main>
     </>
   );
 };
